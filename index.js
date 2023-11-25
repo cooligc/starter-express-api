cd058da6319ae96ca0da1637ca8c76c062d225eb
sitakant <sitakant1991@gmail.com>
2023-11-26 01:44:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
+const schema = require('schema')
+
+
+
 const app = express()
 
 app.get('/', (req, res) => {
   console.log("Just got a request!")
   res.send('Welcome to my-health api');
+});
+
+app.get('/health-metrics', (req, res) => {
+  console.log("loaded schema")
+  res.json(schema);
 });
 
 app.listen(process.env.PORT || 3000)
