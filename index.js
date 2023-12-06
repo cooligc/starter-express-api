84a50050cfc0da266e847b0b70df24cb810e63b4
sitakant <sitakant1991@gmail.com>
2023-12-07 00:23:47 +0530

    added nifty range

--- index.js
+++ index.js
@@ -22,4 +22,49 @@ app.get('/health-metrics', (req, res) => {
   res.json(schema);
 });
 
+
+app.get('/stocks/nifty-range', (req, res) => {
+  let range_type = req.query.range_type
+  let vix = req.query.vix
+  let nifty_close = req.query.nifty_close
+
+  let response = {}
+
+  let range = 1
+
+  switch(range_type){
+    case 'day':
+      range = 365;
+      break;
+    case 'week':
+      range = 52;
+      break;
+    case 'month':
+      range = 12;
+      break;
+  }
+
+
+  
+  let percentage = Math.sqrt(range)/(vix*100) ;
+  let predicted_high = nifty_close + (nifty_close*percentage)
+  let predicted_low = nifty_close - (nifty_close*percentage)
+
+  let _request = {}
+  _request.vix = vix
+  _request.range_type=range_type
+  _request.nifty_close = nifty_close
+
+
+  let _prediction = {} 
+  _prediction.high=predicted_high
+  _prediction.loq=predicted_low
+  _prediction.percentage=percentage
+
+  response.request = _request
+  response.prediction = _prediction
+  
+  res.json(response);
+});
+
 app.listen(process.env.PORT || 3000)
